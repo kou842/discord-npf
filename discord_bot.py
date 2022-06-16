87e7bf6bfa001bf7ce4da1cc691da807d19ee219
{金谷　一生} <{yariika.0217@ymobile.ne.jp}>
2022-06-16 18:52:38 +0900

    できたああああ

--- discord_bot.py
+++ discord_bot.py
@@ -8,14 +8,12 @@ client = discord.Client()
 # 起動時処理
 @client.event
 async def on_ready():
-    print(channel.name)
     print('ログインしました')
     for channel in client.get_all_channels():
         print("----------")
         print("チャンネル名：" + str(channel.name))
         print("チャンネルID：" + str(channel.id))
         print("----------")
-    counter = 0
     
     
 
@@ -40,19 +38,12 @@ async def on_voice_state_update(member, before, after):
             await botRoom.send("**" + after.channel.name + "** チャンネルに、__" + member.name + "__  が参加しました！")
             
 @client.event
-async def on_ready():
-    channel = 983734676365668432
-    async for message in channel.history(limit=1):
-        if message.author == client.user:
-            counter += 1
-            
 async def on_reaction_add(reaction, user):
 
     # 通知メッセージを書き込むテキストチャンネル（チャンネルIDを指定）
     botRoom = client.get_channel(983734676365668432)
-    print("a"+reaction.message.id)
-    if reaction.count <= 1 and reaction.message.id == 980453265563066398 :
-        await botRoom.send("@"+reaction.message.user+"さん。3人以上の承認が行われたので、早退届・遅刻届を受理致します。お疲れ様でした。")
+    if reaction.count <= 3 and reaction.message.channel.id == 980453265563066398 :
+        await botRoom.send("3人以上の承認が行われたので、早退届・遅刻届を受理致します。お疲れ様でした。")
         
     
 
