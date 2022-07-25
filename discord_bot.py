3c9421a2811135c9315a0d586e0d162975e959e4
{金谷　一生} <{yariika.0217@ymobile.ne.jp}>
2022-07-25 23:27:18 +0900

    こみっと

--- discord_bot.py
+++ discord_bot.py
@@ -2,7 +2,7 @@ import discord
 
 TOKEN = 'OTgzNjMyNjg2NjEyNjE1MTg4.GUmqQp.TDAzSFqR8sB3ZaJD_I365_4Kv0AuUyky3eBrsw' # TOKENを貼り付け
 CHANNELID = 983734676365668432 # チャンネルIDを貼り付け
-
+GUILD = 964402354805940225
 client = discord.Client()
 
 # 起動時処理
@@ -14,8 +14,11 @@ async def on_ready():
         print("チャンネル名：" + str(channel.name))
         print("チャンネルID：" + str(channel.id))
         print("----------")
+    guild = client.get_guild(GUILD)
     
-    
+    print(guild.id)
+    print(guild.name)
+    print(guild.members)
 
     
 # チャンネル入退室時の通知処理
@@ -60,11 +63,47 @@ async def on_reaction_add(reaction, user):
                 await botRoom.send("橋本先生のガン萎えを確認しました。"+reaction.message.author.nick+"強制差し戻し致します。「おーとっやらかしたぁ・・・ チっ　もぉー！」")
     else :
         sabun = sabun +1
+    
+    botRoom = client.get_channel(994559802292314173)
+    guild = client.get_guild(GUILD)
+    print ("ここまで来たよやったね！")
+    role = guild.get_role(992298617052348466)#容疑者ID
+    sabun = 0
+    mscotent = reaction.message.content
+    if reaction.emoji.id == 977208213986476093:
+        if reaction.count - sabun == 1 and reaction.message.channel.id == 994559802292314173 :
+            if "社畜ではまい！" in mscotent :
+                memberlist = guild.members
+                for cnt in range(len(memberlist)):
+                    user = memberlist[cnt]
+                    print("ここ")
+                    print(user)
+                    if user.id == 766190879127502858:
+                        user.add_rolse(role)
+                men = client.get_user()
+                print(men)
+                await botRoom.send("3人以上の承認が行われたので、"+men.name+"を逮捕いたします。十分に反省してください。")
+            if "社畜でびっくり！" in mscotent :
+                for cnt in range(len(user)):
+                    user = user[cnt]
+                    if user.id == 607448697827229706:
+                        user.add_rolse(role)
+                        await botRoom.send("3人以上の承認が行われたので、"+reaction.message.author.nick+"を逮捕いたします。十分に反省してください。")
+            if "突き刺さった社畜" in mscotent:
+                await botRoom.send("3人以上の承認が行われたので、"+reaction.message.author.nick+"を逮捕いたします。十分に反省してください。")
+            if "SUNバタコ" in mscotent:
+                await botRoom.send("3人以上の承認が行われたので、"+reaction.message.author.nick+"を逮捕いたします。十分に反省してください。")
+            if "56513" in mscotent:
+                await botRoom.send("3人以上の承認が行われたので、"+reaction.message.author.nick+"を逮捕いたします。十分に反省してください。")
+            if "ゆー者" in mscotent:
+                await botRoom.send("3人以上の承認が行われたので、"+reaction.message.author.nick+"を逮捕いたします。十分に反省してください。")
+        
 @client.event
 async def on_message(message):
     print(message.author.id)
     channel = message.channel
     content = message.content
+    
     
     if channel.id == 986992160719130654:
         if content == "s//":
@@ -76,9 +115,15 @@ async def on_message(message):
         asta = "m//"
         if  asta in content:
             await channel.send("m!play "+comand)
-            
-@client.event
-async def on_message(message):
+    
+    if channel.id == 986992160719130654:
+        idx = content.find('g//')
+        comand = content[idx+3:]  # スライスで半角空白文字のインデックス＋1以降を抽出
+        asta = "g//"
+        if  asta in content:
+            result = "https://game8.jp/genshin/search?q="+comand
+            await channel.send(result)
+
     messager = message.author.roles
     print (messager)
     for number  in range(len(messager)):
@@ -86,5 +131,28 @@ async def on_message(message):
         if role.name == "容疑者（逮捕済み）":
             await message.delete()
     
+    if channel.id == 986992160719130654:
+        we = client.get_channel(994559802292314173)
+        idx = content.find('t//')
+        comand = content[idx+3:]  # スライスで半角空白文字のインデックス＋3以降を抽出
+        asta = "me1"
+        if  asta in comand:
+            await we.send("「社畜ではまい！」に逮捕状が卸されました。逮捕する場合三人以上の承認が必要です。投票お願いします。")
+        asta = "me2"
+        if  asta in comand:
+            await we.send("「社畜でびっくり！」に逮捕状が卸されました。逮捕する場合三人以上の承認が必要です。投票お願いします。")
+        asta = "me3"
+        if  asta in comand:
+            await we.send("「突き刺さった社畜」に逮捕状が卸されました。逮捕する場合三人以上の承認が必要です。投票お願いします。")
+        asta = "me4"
+        if  asta in comand:
+            await we.send("「SUNバタコ」に逮捕状が卸されました。逮捕する場合三人以上の承認が必要です。投票お願いします。")
+        asta = "me5"
+        if  asta in comand:
+            await we.send("「56513」に逮捕状が卸されました。逮捕する場合三人以上の承認が必要です。投票お願いします。")
+        asta = "me6"
+        if  asta in comand:
+            await we.send("「ゆー者」に逮捕状が卸されました。逮捕する場合三人以上の承認が必要です。投票お願いします。")
+
             
 client.run(TOKEN)
